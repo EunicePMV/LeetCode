f3ad10a1701ce018b187a3cfd432434af0ef473a
Eunice Patricia Villanueva <villanueva.eunice.m@gmail.com>
2024-06-20 15:26:41 +0800

    Time: 228 ms (19.35%), Space: 19.8 MB (80.13%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.py
@@ -0,0 +1,14 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        # sort the arr
+        nums.sort()
+        
+        # store final ans
+        final_ans = 0
+
+        # generate the pairs 
+        for i in range(len(nums)//2):
+            final_ans += min(nums[i*2], nums[i*2+1])
+
+        # get the maximized sum
+        return final_ans 
