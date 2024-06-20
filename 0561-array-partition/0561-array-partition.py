e52bf40cf0029ffe02b4cd203dc5b960ee3e6e33
Eunice Patricia Villanueva <villanueva.eunice.m@gmail.com>
2024-06-20 15:33:13 +0800

    Time: 208 ms (90.27%), Space: 19.9 MB (55.98%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,14 +1,17 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         # sort the arr
-        nums.sort()
+        # nums.sort()
         
-        # store final ans
-        final_ans = 0
+        # # store final ans
+        # final_ans = 0
 
-        # generate the pairs 
-        for i in range(len(nums)//2):
-            final_ans += min(nums[i*2], nums[i*2+1])
+        # # generate the pairs 
+        # for i in range(len(nums)//2):
+        #     final_ans += min(nums[i*2], nums[i*2+1])
 
-        # get the maximized sum
-        return final_ans 
+        # # get the maximized sum
+        # return final_ans 
+
+        nums.sort()
+        return sum(nums[::2])
